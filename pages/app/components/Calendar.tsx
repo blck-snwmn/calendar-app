a61a1d497e8f6d610845d1cb0b8a35a0a1b3a50e
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-12 14:56:08 +0900

    Use emotion

--- pages/app/components/Calendar.tsx
+++ pages/app/components/Calendar.tsx
@@ -1,12 +1,49 @@
-// biome-ignore lint/style/useImportType: <explanation>
-import React, { useMemo } from "react";
-
-import styles from './Calendar.module.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import type React from 'react';
 
 type CalendarProps = {
     year: number;
     month: number; // 月は0基準で、0が1月、11が12月です。
 };
+
+const calendarContainerStyle = css`
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px;
+    justify-content: start;
+    width: 100%;
+`;
+
+const weekdayStyle = css`
+    flex: 0 0 14%;
+    height: 30px; /* 曜日行の高さ */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f0f0f0; /* 曜日背景色 */
+`;
+
+const dayStyle = css`
+    flex: 0 0 14%;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    height: 100px;
+    margin: 0;
+    background-color: #fff;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+    transition: background-color 0.2s;
+    &:hover {
+        background-color: #e0e0e0;
+    }
+`;
+
+const emptyDayStyle = css`
+    flex: 0 0 14%;
+    height: 100px;
+    margin: 0;
+`;
 
 const daysInMonth = (year: number, month: number) => {
     return new Date(year, month + 1, 0).getDate();
@@ -14,24 +51,21 @@ const daysInMonth = (year: number, month: number) => {
 
 const Calendar: React.FC<CalendarProps> = ({ year, month }) => {
     const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
     const numDays = daysInMonth(year, month);
     const startDay = new Date(year, month, 1).getDay();
-
     const days = Array.from({ length: numDays }, (_, i) => i + 1);
-    const emptyDays: (number | null)[] = Array.from({ length: startDay }, () => null);
-    const totalCells = emptyDays.concat(days);
+    const emptyDays = Array.from({ length: startDay }, () => null);
 
     return (
-        <div className={styles.calendarContainer}>
+        <div css={calendarContainerStyle}>
             {weekdays.map(weekday => (
-                <div key={weekday} className={styles.weekday}>{weekday}</div>
+                <div key={weekday} css={weekdayStyle}>{weekday}</div>
             ))}
-            {emptyDays.map((_, index) => (
-                <div key={`empty-${year}-${month}`} className={styles.emptyDay} />
+            {emptyDays.map(() => (
+                <div key={`empty-${year}-${month}`} css={emptyDayStyle} />
             ))}
             {days.map(day => (
-                <div key={`day-${year}-${month}-${day}`} className={styles.day}>
+                <div key={`day-${year}-${month}-${day}`} css={dayStyle}>
                     {day}
                 </div>
             ))}
