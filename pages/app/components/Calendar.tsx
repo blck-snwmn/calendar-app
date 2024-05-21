e7d33346cc01144b7e65950bae6f2f07fd7c9c5e
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-22 02:05:19 +0900

    Omit if the number of appointments is large.

--- pages/app/components/Calendar.tsx
+++ pages/app/components/Calendar.tsx
@@ -29,27 +29,34 @@ const Calendar: React.FC<CalendarProps> = ({
 					{weekday}
 				</div>
 			))}
-			{allDates.map(({ day, isHoliday, events, key }) => (
-				<div
-					key={key}
-					className={`relative flex-0 flex-grow-[0] flex-shrink-[0] w-[14%] flex flex-col items-center justify-start h-[200px] m-0 bg-white shadow-sm transition-colors gap-2 p-2 box-border ${isHoliday ? "text-red-500" : ""}`}
-				>
-					<div>{day}</div>
-					{events.map(event => (
-						event.title ? (
-							<Link
-								key={event.id}
-								to={`${event.id}`}
-								className="text-xs h-[24px] p-1 mb-1 rounded bg-gray-200 shadow-xs whitespace-nowrap overflow-hidden text-ellipsis w-full box-border"
-							>
-								{event.title}
-							</Link>
-						) : (
-							<div key={event.id} className="h-[24px] w-full mb-1 bg-transparent" />
-						)
-					))}
-				</div>
-			))}
+			{allDates.map(({ day, isHoliday, events, key }) => {
+				const displayEvents = events.slice(0, 4); // 最初の4つの予定を表示
+				const hasMoreEvents = events.length > 4;  // 5つ以上の予定があるかどうか
+				return (
+					<div
+						key={key}
+						className={`relative flex-0 flex-grow-[0] flex-shrink-[0] w-[14%] flex flex-col items-center justify-start h-[220px] m-0 bg-white shadow-sm transition-colors gap-2 p-2 box-border ${isHoliday ? "text-red-500" : ""}`}
+					>
+						<div>{day}</div>
+						{displayEvents.map(event => (
+							event.title ? (
+								<Link
+									key={event.id}
+									to={`${event.id}`}
+									className="text-xs h-[24px] p-1 mb-1 rounded bg-gray-200 shadow-xs whitespace-nowrap overflow-hidden text-ellipsis w-full box-border"
+								>
+									{event.title}
+								</Link>
+							) : (
+								<div key={event.id} className="h-[24px] w-full mb-1 bg-transparent" />
+							)
+						))}
+						{hasMoreEvents && (
+							<div className="text-xs text-gray-500">+{events.length - 4} more</div>
+						)}
+					</div>
+				);
+			})}
 		</div>
 	);
 };
